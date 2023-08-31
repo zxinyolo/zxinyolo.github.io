9d02f06fbc03f144d87db002427bd3165d6d0eea
zxinyolo <1129170176@qq.com>
2023-08-31 14:56:21 +0800

    new

--- update_doc.sh
+++ update_doc.sh
@@ -4,14 +4,17 @@
 hexo_output=$(hexo g)
 wait
 echo "$hexo_output"
+echo "############################################"
 
 git_add=$(git add .)
 wait
 echo "$git_add"
+echo "############################################"
 
 git_commit=$(git ci "new")
 wait
 echo "$git_commit"
+echo "############################################"
 
 git_push=$(git push origin main)
 wait
